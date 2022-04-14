4905e1f2da7af92521e159996600488de10fab16
BS-CURTIS <curtis@buyingsquare.co.kr>
2022-04-14 18:52:24 +0900

    설정 변경

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  presets: ["next/babel"],
   reactStrictMode: true,
   images: {
     domains: ["yts.torrentbay.to"],
   },
-  plugins: [["babel-plugin-styled-components"]],
 };
 
 module.exports = nextConfig;
