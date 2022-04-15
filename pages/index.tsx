f0bc7131915a798aec46b17e778b68a8269f2092
BS-CURTIS <curtis@buyingsquare.co.kr>
2022-04-15 10:39:15 +0900

    fetch  설정 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,15 @@ const Index = ({ movies }: movies) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const res = await fetch(
-    "https://yts.torrentbay.to/api/v2/list_movies.json?quality=2160p&sort_by=rating"
+    "https://yts.torrentbay.to/api/v2/list_movies.json?quality=2160p&sort_by=rating",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "User-Agent": "*",
+      },
+    }
   );
   const json = await res.json();
   const movies = json.data.movies;
